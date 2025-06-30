2364c6a5cec8ee10c8aa28800eebdc1ac85177ba
RiyAndhi <128784894+RiyAndhi@users.noreply.github.com>
2025-07-01 00:01:45 +0700

    Update and rename service-worker.js to sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', event => {
+  console.log('[ServiceWorker] Installed');
+  event.waitUntil(
+    caches.open('pwa-redirect-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
